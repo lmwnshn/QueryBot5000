534e9768eae2ba31e12d19e2a3aef8a7452b264f
Wan Shen Lim <wanshen.lim@gmail.com>
2021-12-19 15:39:04 -0500

    Checkpoint.

--- preprocessor.py
+++ preprocessor.py
@@ -1,7 +1,17 @@
+import glob
+
+from pandarallel import pandarallel
+
+from multiprocessing import Pool
+
+from tqdm.contrib.concurrent import process_map
+
+import numpy as np
 import pandas as pd
 import pglast
 import time
 
+pandarallel.initialize()
 
 class Preprocessor:
   # https://www.postgresql.org/docs/13/runtime-config-logging.html#RUNTIME-CONFIG-LOGGING-CSVLOG
@@ -32,36 +42,36 @@ class Preprocessor:
     'backend_type',
   ]
 
+  def get_dataframe():
+    return self._df
+
+  def get_grouped_dataframe():
+    return self._grouped_df
+
+  @staticmethod
+  def _read_csv(csvlog):
+    # This function must have a separate non-local binding from _read_df
+    # so that it is pickleable.
+    return pd.read_csv(
+      csvlog,
+      names=Preprocessor.PG_LOG_COLUMNS,
+      parse_dates=['log_time', 'session_start_time'],
+      usecols=['log_time', 'session_start_time', 'command_tag', 'message', 'detail'],
+      header=None,
+      index_col=False,
+    )
+
   @staticmethod
   def _read_df(csvlogs):
-    df = pd.concat(
-      pd.read_csv(
-        csvlog,
-        names=Preprocessor.PG_LOG_COLUMNS,
-        parse_dates=['log_time', 'session_start_time'],
-        usecols=['log_time', 'session_start_time', 'command_tag', 'message', 'detail'],
-        header=None,
-        index_col=False,
-      )
-      for csvlog in csvlogs
-    )
-    return df
+    return pd.concat(process_map(Preprocessor._read_csv, csvlogs))
 
   @staticmethod
   def _extract_query(message_series):
-    query = message_series.str.extract('((?:DELETE|INSERT|SELECT|UPDATE).*)')
-    print(query)
-    return query.apply(pglast.prettify, axis=1)
-    commands = ['SELECT', 'INSERT', 'UPDATE', 'DELETE']
-    def extract(message):
-      for command in commands:
-        idx = message.find(command)
-        if idx != -1:
-          query = message[idx:]
-          # Standardize query format.
-          return pglast.prettify(query)
-      return ''
-    return message_series.apply(extract)
+    query = message_series.str.extract('((?:DELETE|INSERT|SELECT|UPDATE).*)', expand=False)
+    print('TODO(WAN): expose prettify option as command line. skipping that.')
+    #query = query.parallel_map(pglast.prettify, na_action='ignore')
+    query.fillna('', inplace=True)
+    return query.astype(str)
 
   @staticmethod
   def _extract_params(detail_series):
@@ -79,7 +89,7 @@ class Preprocessor:
         assert pnum[1:].isdigit()
         params[pnum] = pval
       return params
-    return detail_series.apply(extract)
+    return detail_series.parallel_apply(extract)
 
   @staticmethod
   def _substitute_params(df, query_col, params_col):
@@ -87,8 +97,8 @@ class Preprocessor:
       for k, v in params.items():
         query = query.replace(k, v)
       return query
-    return df.apply(lambda row: substitute(row[query_col], row[params_col]),
-                    axis=1)
+    return df.parallel_apply(
+      lambda row: substitute(row[query_col], row[params_col]), axis=1)
 
   @staticmethod
   def _parse(query_series):
@@ -98,44 +108,69 @@ class Preprocessor:
         token_str = sql[token.start:token.end + 1]
         if token.start > last_end:
           new_sql.append(' ')
-        if token.name in ['ICONST', 'FCONST']:
-          # Integer or float constant.
-          new_sql.append('#')
-          params.append(token_str)
-        elif token.name in ['SCONST']:
-          # String constant.
-          new_sql.append('&&&')
+        if token.name in ['ICONST', 'FCONST', 'SCONST']:
+          # Integer, float, or string constant.
+          new_sql.append('$' + str(len(params) + 1))
           params.append(token_str)
         else:
           new_sql.append(token_str)
         last_end = token.end + 1
       new_sql = ''.join(new_sql)
       return new_sql, params
-    return query_series.apply(parse)
+    return query_series.parallel_apply(parse)
 
   def __init__(self, csvlogs):
-    time_old, time_new = time.time_ns(), time.time_ns()
+    time_end, time_start = None, time.perf_counter()
+
+    def clock(label):
+      nonlocal time_end, time_start
+      time_end = time.perf_counter()
+      print('\r{}: {:.2f} s'.format(label, time_end - time_start))
+      time_start = time_end
+
     df = self._read_df(csvlogs)
+    clock('Read dataframe')
 
-    print('Extract query ')
+    print('Extract queries: ', end='', flush=True)
     df['query_raw'] = self._extract_query(df['message'])
-    time_old, time_new = time_new, time.time_ns()
-    print(time_new - time_old, ' ns')
+    clock('Extract queries')
 
-    print('Extract params', end='')
+    print('Extract parameters: ', end='', flush=True)
     df['params'] = self._extract_params(df['detail'])
-    print('Subst params into query')
+    clock('Extract parameters')
+
+    print('Substitute parameters into query: ', end='', flush=True)
     df['query_subst'] = self._substitute_params(df, 'query_raw', 'params')
-    print('Parse query')
-    df['query'] = self._parse(df['query_subst'])
-    print(df['query'].iloc[0])
+    clock('Substitute parameters into query')
+
+    print('Parse query: ', end='', flush=True)
+    parsed = self._parse(df['query_subst'])
+    df[['query_template', 'query_params']] = pd.DataFrame(parsed.tolist(), index=df.index)
+    clock('Parse query')
+
+    # Round all times to closest second.
+    df['log_time_s'] = df['log_time'].round('S')
+
+    gb = df.groupby(['query_template', 'log_time_s']).size()
+    grouped_df = pd.DataFrame(gb, columns=['count'])
+    grouped_df.drop('', axis=0, level=0, inplace=True)
+
+    self._df = df
+    self._grouped_df = grouped_df
 
 
+def main():
+  pgfiles = [
+   'data/extracted/simple/postgresql-2021-12-06_160140.csv',
+   'data/extracted/extended/postgresql-2021-12-06_142428.csv',
+  ]
+
+  pgfiles = []
+  pgfiles += glob.glob('data/extracted/simple/postgresql*.csv')
+  pgfiles += glob.glob('data/extracted/extended/postgresql*.csv')
+
+  Preprocessor(pgfiles)
 
 
-pgfiles = [
- 'data/extracted/simple/postgresql-2021-12-06_160140.csv',
- 'data/extracted/extended/postgresql-2021-12-06_142428.csv',
-]
-
-Preprocessor(pgfiles)
+if __name__ == '__main__':
+  main()
